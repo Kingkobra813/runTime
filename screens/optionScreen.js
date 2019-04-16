c15f311146ca3f5c10032cd751c8c1d4d0aaa1c2
AdamSmelly <adam5689@msn.com>
2019-04-15 19:04:05 -0600

    made the basic structure for options screen

--- screens/optionScreen.js
+++ screens/optionScreen.js
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
+import { Dropdown } from "react-native-material-dropdown";
 
-export default class App extends React.Component {
+class OptionScreen extends Component {
+  handleStartRunning = () => {
+    console.log("startRunning");
+  };
+
   render() {
+    let runningTime = [{ value: "1 Minute" }, { value: "2 Minutes" }];
+    let walkingTime = [{ value: "1 Minute" }, { value: "2 minutes" }];
     return (
-      <View style={styles.container}>
-        <Text>Make a dropdown</Text>
+      <View>
+        <Text>Running Time</Text>
+        <Dropdown label="Running Time" data={runningTime} />
+        <Text>Walking Time</Text>
+        <Dropdown label="Walking Time" data={walkingTime} />
+        <Button title="Start running" onPress={this.handleStartRunning} />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+export default OptionScreen;
