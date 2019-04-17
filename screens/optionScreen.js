afea742c08423a2a1087bb9b5d2956c5333c6608
AdamSmelly <adam5689@msn.com>
2019-04-16 18:51:25 -0600

    working timer

--- screens/optionScreen.js
+++ screens/optionScreen.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 import { Dropdown } from "react-native-material-dropdown";
+import TimerCountdown from "react-native-timer-countdown";
 
 class OptionScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      startTime: "",
+      isPressed: false
+    };
+  }
   handleStartRunning = () => {
     console.log("startRunning");
   };
@@ -17,6 +26,30 @@ class OptionScreen extends Component {
         <Text>Walking Time</Text>
         <Dropdown label="Walking Time" data={walkingTime} />
         <Button title="Start running" onPress={this.handleStartRunning} />
+        <TimerCountdown
+          initialMilliseconds={this.state.isPressed ? 1000 * 60 : 1000 * 0}
+          onTick={milliseconds => console.log("tick", milliseconds)}
+          onExpire={() => console.log("complete")}
+          formatMilliseconds={milliseconds => {
+            const remainingSec = Math.round(milliseconds / 1000);
+            const seconds = parseInt((remainingSec % 60).toString(), 10);
+            const minutes = parseInt(((remainingSec / 60) % 60).toString(), 10);
+            const hours = parseInt((remainingSec / 3600).toString(), 10);
+            const s = seconds < 10 ? "0" + seconds : seconds;
+            const m = minutes < 10 ? "0" + minutes : minutes;
+            let h = hours < 10 ? "0" + hours : hours;
+            h = h === "00" ? "" : h + ":";
+            return h + m + ":" + s;
+          }}
+          allowFontScaling={true}
+          style={{ fontSize: 20 }}
+        />
+        <Button
+          title={`${this.state.isPressed ? "Running" : "Start running"}`}
+          onPress={() => {
+            this.setState({ isPressed: true });
+          }}
+        />
       </View>
     );
   }
