de68bac7a134f27550bd2de35f67c2f8b0e2cdd9
AdamSmelly <adam5689@msn.com>
2019-04-19 18:57:41 -0600

    basic styling

--- screens/optionScreen.js
+++ screens/optionScreen.js
@@ -31,79 +31,93 @@ class OptionScreen extends Component {
     let runningTime = [{ value: "1 Minute" }, { value: "2 Minutes" }];
     let walkingTime = [{ value: "1 Minute" }, { value: "2 minutes" }];
     return (
-      <View>
-        <Text>Running Time</Text>
+      <View style={[{ width: "90%", justifyContent: "center", margin: 20 }]}>
         <Dropdown
           label="Running Time"
           data={runningTime}
           onChangeText={value => this.handleStartRunning(value)}
         />
-        <Text>Walking Time</Text>
         <Dropdown
           label="Walking Time"
           data={walkingTime}
           onChangeText={value => this.handleStartWalking(value)}
         />
-        <TimerCountdown
-          initialMilliseconds={
-            this.state.runIsPressed ? this.state.runTime : 1000 * 0
-          }
-          onTick={milliseconds => console.log("tick", milliseconds)}
-          onExpire={() =>
-            this.setState({ walkIsPressed: true, runIsPressed: false })
-          }
-          formatMilliseconds={milliseconds => {
-            const remainingSec = Math.round(milliseconds / 1000);
-            const seconds = parseInt((remainingSec % 60).toString(), 10);
-            const minutes = parseInt(((remainingSec / 60) % 60).toString(), 10);
-            const hours = parseInt((remainingSec / 3600).toString(), 10);
-            const s = seconds < 10 ? "0" + seconds : seconds;
-            const m = minutes < 10 ? "0" + minutes : minutes;
-            let h = hours < 10 ? "0" + hours : hours;
-            h = h === "00" ? "" : h + ":";
-            return h + m + ":" + s;
-          }}
-          allowFontScaling={true}
-          style={{ fontSize: 20 }}
-        />
-        <TimerCountdown
-          initialMilliseconds={
-            this.state.walkIsPressed ? this.state.walkTime : 1000 * 0
-          }
-          onTick={milliseconds => console.log("tick", milliseconds)}
-          onExpire={() =>
-            this.setState({
-              runIsPressed: true,
-              walkIsPressed: false,
-              runCount: +1
-            })
-          }
-          formatMilliseconds={milliseconds => {
-            const remainingSec = Math.round(milliseconds / 1000);
-            const seconds = parseInt((remainingSec % 60).toString(), 10);
-            const minutes = parseInt(((remainingSec / 60) % 60).toString(), 10);
-            const hours = parseInt((remainingSec / 3600).toString(), 10);
-            const s = seconds < 10 ? "0" + seconds : seconds;
-            const m = minutes < 10 ? "0" + minutes : minutes;
-            let h = hours < 10 ? "0" + hours : hours;
-            h = h === "00" ? "" : h + ":";
-            return h + m + ":" + s;
-          }}
-          allowFontScaling={true}
-          style={{ fontSize: 20 }}
-        />
-        <Button
-          title={`${this.state.runIsPressed ? "Running" : "Start running"}`}
-          onPress={() => {
-            this.setState({ runIsPressed: true });
-          }}
-        />
-        <Button
-          title="Stop"
-          onPress={() => {
-            this.setState({ runIsPressed: false, walkIsPressed: false });
-          }}
-        />
+        <View
+          style={[
+            { justifyContent: "center", alignItems: "center", padding: 10 }
+          ]}
+        >
+          <TimerCountdown
+            initialMilliseconds={
+              this.state.runIsPressed ? this.state.runTime : 1000 * 0
+            }
+            onTick={milliseconds => console.log("tick", milliseconds)}
+            onExpire={() =>
+              this.setState({ walkIsPressed: true, runIsPressed: false })
+            }
+            formatMilliseconds={milliseconds => {
+              const remainingSec = Math.round(milliseconds / 1000);
+              const seconds = parseInt((remainingSec % 60).toString(), 10);
+              const minutes = parseInt(
+                ((remainingSec / 60) % 60).toString(),
+                10
+              );
+              const hours = parseInt((remainingSec / 3600).toString(), 10);
+              const s = seconds < 10 ? "0" + seconds : seconds;
+              const m = minutes < 10 ? "0" + minutes : minutes;
+              let h = hours < 10 ? "0" + hours : hours;
+              h = h === "00" ? "" : h + ":";
+              return h + m + ":" + s;
+            }}
+            allowFontScaling={true}
+            style={{ fontSize: 70 }}
+          />
+          <TimerCountdown
+            initialMilliseconds={
+              this.state.walkIsPressed ? this.state.walkTime : 1000 * 0
+            }
+            onTick={milliseconds => console.log("tick", milliseconds)}
+            onExpire={() =>
+              this.setState({
+                runIsPressed: true,
+                walkIsPressed: false,
+                runCount: +1
+              })
+            }
+            formatMilliseconds={milliseconds => {
+              const remainingSec = Math.round(milliseconds / 1000);
+              const seconds = parseInt((remainingSec % 60).toString(), 10);
+              const minutes = parseInt(
+                ((remainingSec / 60) % 60).toString(),
+                10
+              );
+              const hours = parseInt((remainingSec / 3600).toString(), 10);
+              const s = seconds < 10 ? "0" + seconds : seconds;
+              const m = minutes < 10 ? "0" + minutes : minutes;
+              let h = hours < 10 ? "0" + hours : hours;
+              h = h === "00" ? "" : h + ":";
+              return h + m + ":" + s;
+            }}
+            allowFontScaling={true}
+            style={{ fontSize: 70 }}
+          />
+        </View>
+        <View style={[{ justifyContent: "center", padding: 40 }]}>
+          <Button
+            color="green"
+            title={`${this.state.runIsPressed ? "Running" : "Start running"}`}
+            onPress={() => {
+              this.setState({ runIsPressed: true });
+            }}
+          />
+          <Button
+            title="Stop"
+            color="#ff5c5c"
+            onPress={() => {
+              this.setState({ runIsPressed: false, walkIsPressed: false });
+            }}
+          />
+        </View>
       </View>
     );
   }
